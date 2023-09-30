4fe3fa1457def56b4f17cb3471eeae83f5e04feb
Prabodashan <prabodashan14@gmail.com>
2023-09-30 09:47:16 +0530

    Added Redux Thunks for send request

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,15 +1,30 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const initialState = [
-  { id: "0", name: "Dude Lebowski" },
-  { id: "1", name: "Neil Young" },
-  { id: "2", name: "Dave Gray" },
-];
+const USERS_URL = "https://jsonplaceholder.typicode.com/users";
+
+const initialState = [];
+
+// const initialState = [
+//   { id: "0", name: "Dude Lebowski" },
+//   { id: "1", name: "Neil Young" },
+//   { id: "2", name: "Dave Gray" },
+// ];
+
+export const fetchUsers = createAsyncThunk("users/fetchUsers", async () => {
+  const response = await axios.get(USERS_URL);
+  return response.data;
+});
 
 const usersSlice = createSlice({
   name: "users",
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      return action.payload;
+    });
+  },
 });
 
 export const selectAllUsers = (state) => state.users;
